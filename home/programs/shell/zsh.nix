cf1e2cb9804ba00c8c07f66106d2baecde136882
wheelibin <wheelibin@gmail.com>
2025-11-21 14:44:39 +0000

    move alias

--- home/programs/shell/zsh.nix
+++ home/programs/shell/zsh.nix
@@ -10,14 +10,14 @@
       plugins = [ ];
     };
 
+    shellAliases.l = "eza -l";
+
     initContent = ''
       # Fix TERM from Ghostty when SSH-ing
       if [[ "$TERM" = "xterm-ghostty" ]]; then
         export TERM="xterm-256color"
       fi
       export EDITOR="nvim"
-      alias ll="eza -l"
-      alias gs="git status"
       
       export ELECTRON_ENABLE_WAYLAND=1
     '';
