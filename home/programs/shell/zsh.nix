0be658de33c799905df786843914be95e07e82ed
wheelibin <wheelibin@gmail.com>
2025-11-22 10:35:21 +0000

    tidy up

--- home/programs/shell/zsh.nix
+++ home/programs/shell/zsh.nix
@@ -10,14 +10,5 @@
       plugins = [ ];
     };
 
-    initContent = ''
-      # Fix TERM from Ghostty when SSH-ing
-      if [[ "$TERM" = "xterm-ghostty" ]]; then
-        export TERM="xterm-256color"
-      fi
-      export EDITOR="nvim"
-
-      export ELECTRON_ENABLE_WAYLAND=1
-    '';
   };
 }
