e1726c242ec3aa430428ad78f518907201f3c54f
Jon Wheeler <jon.wheeler@astrazeneca.com>
2025-12-10 12:03:58 +0000

    update to 25.11

--- flake.nix
+++ flake.nix
@@ -2,12 +2,12 @@
   description = "NixOS configuration with multiple machines and user profiles";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     nixos-hardware.url = "github:NixOS/nixos-hardware";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
