cb68d5ae0540fe200bb1f35585a1fa80a8e7202a
wheelibin <wheelibin@gmail.com>
2025-12-11 10:23:44 +0000

    add kenkufm

--- pkgs/kenku-fm.nix
+++ pkgs/kenku-fm.nix
@@ -1,5 +1,7 @@
-{ lib, stdenv, fetchurl, autoPatchelfHook, dpkg, alsa-lib, libX11, libXcursor
-, libXi, libXrandr, libXrender, libXScrnSaver, libXtst, nss, gtk3 }:
+{ lib, stdenv, fetchurl, autoPatchelfHook, binutils # for `ar`
+, alsa-lib, libX11, libXcursor, libXi, libXrandr, libXrender, libXScrnSaver
+, libXtst, nss, gtk3, mesa # NEW
+}:
 
 stdenv.mkDerivation rec {
   pname = "kenku-fm";
@@ -8,11 +10,10 @@ stdenv.mkDerivation rec {
   src = fetchurl {
     url =
       "https://github.com/owlbear-rodeo/kenku-fm/releases/download/v${version}/kenku-fm_${version}_amd64.deb";
-    # From the official GitHub release page
     sha256 = "888b56c7eaefd2339305c3da6b789e09fdbc388fc7bdc9d18c6ab1130d0ed1c5";
   };
 
-  nativeBuildInputs = [ autoPatchelfHook dpkg ];
+  nativeBuildInputs = [ autoPatchelfHook binutils ];
 
   buildInputs = [
     alsa-lib
@@ -25,22 +26,24 @@ stdenv.mkDerivation rec {
     libXtst
     nss
     gtk3
+    mesa # NEW: provides libgbm.so.1
   ];
 
   dontBuild = true;
 
   unpackPhase = ''
-    dpkg-deb -x $src .
+    ar x "$src"
+    tar --no-same-owner --no-same-permissions -xf data.tar.*
   '';
 
   installPhase = ''
-    mkdir -p $out
-    # Copy files from the .deb payload
-    cp -r "opt/Kenku FM"/* $out/
+    mkdir -p "$out"
 
-    # Provide a nice binary on PATH
-    mkdir -p $out/bin
-    ln -s $out/kenku-fm $out/bin/kenku-fm
+    # .deb payload goes into usr/lib/kenku-fm
+    cp -r usr/lib/kenku-fm/* "$out/"
+
+    mkdir -p "$out/bin"
+    ln -s "$out/kenku-fm" "$out/bin/kenku-fm"
   '';
 
   meta = with lib; {
