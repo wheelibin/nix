a3bcac39e157337caec45a5ddf7ea9af4d5e1e0f
wheelibin <wheelibin@gmail.com>
2026-02-16 08:13:05 +0000

    fix: global npm install and npm link

--- home/programs/languages/node.nix
+++ home/programs/languages/node.nix
@@ -1,11 +1,28 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 
+let
+  npmPrefix = "${config.home.homeDirectory}/.npm-global";
+  npmCache = "${config.home.homeDirectory}/.npm-cache";
+in
 {
-
   home.packages = with pkgs; [
     nodejs_24
     eslint_d
     prettier
   ];
 
+  # Create dirs during activation (no lib.dag dependency)
+  home.activation.ensureNpmPrefix = ''
+    mkdir -p ${npmPrefix}/bin ${npmPrefix}/lib ${npmPrefix}/node_modules
+    mkdir -p ${npmCache}
+  '';
+
+  home.file.".npmrc".text = ''
+    prefix=${npmPrefix}
+    cache=${npmCache}
+  '';
+
+  home.sessionPath = [
+    "${npmPrefix}/bin"
+  ];
 }
