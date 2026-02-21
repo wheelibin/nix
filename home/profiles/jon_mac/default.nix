729182c18c96c347d7c205c37eeb2558a948fe35
wheelibin <wheelibin@gmail.com>
2026-02-21 16:11:55 +0000

    add claude to jon_mac profile

--- home/profiles/jon_mac/default.nix
+++ home/profiles/jon_mac/default.nix
@@ -5,6 +5,7 @@
     ../common/darwin.nix
     ./packages.nix
     ../../programs/desktop/karabiner-elements.nix
+    ../../programs/ai/claude.nix
   ];
 
   programs.zsh.shellAliases.hm = "home-manager switch --impure --flake ~/dev/nix#jon_mac";
