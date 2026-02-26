9774ad487053ab623cdffb79dcd81ef0795a3f76
wheelibin <wheelibin@gmail.com>
2026-02-26 21:28:26 +0000

    add local git config include

--- modules/git/default.nix
+++ modules/git/default.nix
@@ -5,7 +5,15 @@
     ./lazygit.nix
   ];
 
-  programs.git.enable = true;
+  programs.git = {
+    enable = true;
+    settings = {
+      include = {
+        path = "~/.config/git/local.config";
+      };
+      pull.rebase = true;
+    };
+  };
 
   home.packages = with pkgs; [
     git-lfs
