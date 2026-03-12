24729b7af15f250b29748233fbb48c629ceaa2fc
wheelibin <wheelibin@gmail.com>
2026-03-12 08:59:33 +0000

    add aws cli

--- profiles/mac_work/default.nix
+++ profiles/mac_work/default.nix
@@ -7,6 +7,7 @@
     ../../modules/desktop/bruno.nix
     ../../modules/desktop/karabiner-elements.nix
     ../../modules/ai/opencode.nix
+    ../../modules/cloud/aws.nix
   ];
 
   programs.zsh.shellAliases.hm = "home-manager switch --impure --flake ~/dev/nix#mac_work";
