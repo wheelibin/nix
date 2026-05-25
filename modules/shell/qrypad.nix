5aecb4efda96d29b9f947ef638265bda60f12581
wheelibin <wheelibin@gmail.com>
2026-05-25 08:36:06 +0100

    update qrypad

--- modules/shell/qrypad.nix
+++ modules/shell/qrypad.nix
@@ -3,11 +3,11 @@
 let
   qrypad = pkgs.stdenvNoCC.mkDerivation {
     pname = "qrypad";
-    version = "1.6.0";
+    version = "1.7.0";
 
     src = pkgs.fetchurl {
-      url = "https://github.com/wheelibin/qrypad/releases/download/v1.6.0/qrypad_1.6.0_darwin_arm64.tar.gz";
-      hash = "sha256-5DG9rhdHGJRswC0EwS/K1wdYFgNxr0YiYbhMmynABKY=";
+      url = "https://github.com/wheelibin/qrypad/releases/download/v1.7.0/qrypad_1.7.0_darwin_arm64.tar.gz";
+      hash = "sha256-pxtZ5Dew0YT2+U5akVfL5c5iQ/AqNuJbuo7KZENYJSg=";
     };
 
     sourceRoot = ".";
